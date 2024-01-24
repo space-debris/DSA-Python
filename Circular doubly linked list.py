9139d62dff812aa079740f21b26b19b38191c0b9
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-01-24 20:42:38 +0530

    cdll

--- Circular doubly linked list.py
+++ Circular doubly linked list.py
@@ -26,10 +26,52 @@ class CDLL:
         if self.isempty():
             nn.prev=nn
             nn.next=nn
+            self.start=nn
         else:
+            nn.next=self.start
             nn.prev=self.start.prev
             self.start.prev.next=nn
-            nn.next=self.start
             self.start.prev=nn
+    def search(self,data):
+        temp=self.start
+        if temp==None:
+            return None
+        if temp.item==data:
+            return temp
+        else:
+            temp=temp.next
+        while temp != self.start:
+            if temp.item==data:
+                return temp
+            temp=temp.next
+    def searchtest(self,data):
+        temp=self.start
+        if temp==None:
+            return None
+        while temp.next != self.start:
+            if temp.item==data:
+                return temp
+            temp=temp.next
+    def insertafter(self,temp,data):
+        if temp is not None:
+            nn=Node(temp,data,temp.next)
+            temp.next=nn
+            temp.next.prev=nn
+    def printcdll(self):
+        if not self.isempty():
+            temp=self.start
+            while temp.next is not self.start:
+                print(temp.item,end=" ")
+                temp=temp.next
+            print(temp.item)
+    def deletefirst(self):
+        if not self.isempty():
+            if self.start.prev==self.start.next:
+                self.start=None
+            else:
+                self.start.prev.next=self.start.next
+                self.start.next.prev=self.start.prev
+            
+
         
             
