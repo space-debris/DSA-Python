59e40f5b31ebeea11f9c6aeb82426b3bd67b65c0
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-01-25 16:39:13 +0530

    push

---
+++ Stack using list.py
@@ -0,0 +1,11 @@
+class Stack:
+    def __init__(self):
+        self.items=[]
+    def is_empty(self):
+        return len(self.item)==0
+    def push(self,data):
+        self.items.append(data)
+    def pop(self):
+        self.items
+        
+    
