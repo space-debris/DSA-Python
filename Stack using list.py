8b4dea137900b7fe4250ecfe1c89f60f778ac90e
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-01-25 17:22:25 +0530

    stack

--- Stack using list.py
+++ Stack using list.py
@@ -2,7 +2,7 @@ class Stack:
     def __init__(self):
         self.items=[]
     def is_empty(self):
-        return len(self.item)==0
+        return len(self.items)==0
     def push(self,data):
         self.items.append(data)
     def pop(self):
@@ -12,12 +12,17 @@ class Stack:
             raise IndexError("stack is empty")
     def peek(self):
         if not self.is_empty():
-            return self.item[-1]
+            return self.items[-1]
         else:
             raise IndexError("stack is empty")
     def size(self):
         return len(self.items)
     
-      
-    
+stack = Stack()
+stack.push(10)
+stack.pop()
+stack.push(20)
+print(stack.is_empty())
+print(stack.size())
+print(stack.peek())
     
