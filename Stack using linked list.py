99fc0bdec63cd138e19bfc5afefd34f061698a5e
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-01-26 20:59:57 +0530

    stack

--- Stack using linked list.py
+++ Stack using linked list.py
@@ -6,7 +6,7 @@ class Node:
 class Stack:
     def __init__(self):
         self.top=None
-        self.item_count=None
+        self.item_count=0
     def is_empty(self):
         return self.top == None
     def push(self,data):
@@ -15,12 +15,26 @@ class Stack:
         self.item_count +=1
     def pop(self):
         if not self.is_empty():
-            data = self.start.item
-            self.start=self.start.next
-            self.item.count -=1
+            data = self.top.item
+            self.top=self.top.next
+            self.item_count -=1
             return data
         else:
             raise IndexError("stack is empty")
     def peek(self):
         if not self.is_empty():
-            return self.start.item
+            return self.top.item
+        else:
+            raise self.is_empty()
+    def size(self):
+        return self.item_count
+
+s= Stack()
+s.push(10)
+s.push(20)
+s.push(30)
+print("total elements iin the stack",s.size())
+print("top element in the stack is ", s.peek())
+print("poped element is",s.pop())
+print("total elements iin the stack",s.size())
+print("top element in the stack is ", s.peek())
