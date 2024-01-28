a2fa6fa50fc47b3f564ee9997c14e4c51966ef76
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-01-28 17:52:40 +0530

    first commit

---
+++ Queue by extending linked list.py
@@ -0,0 +1,10 @@
+from Singly_linked_list import *
+
+class Queue(SLL):
+    def __init__(self):
+        super.__init__()
+        self.item_count = 0
+    
+    def is_empty(self):
+        return super().is_empty()
+    
