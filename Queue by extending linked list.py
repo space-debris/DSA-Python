9a759480005d67fb1be1a9329bccf2e0e32cca40
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-01-28 18:01:21 +0530

    second commit

--- Queue by extending linked list.py
+++ Queue by extending linked list.py
@@ -4,7 +4,30 @@ class Queue(SLL):
     def __init__(self):
         super.__init__()
         self.item_count = 0
+        # self.front = None
+        # self.rear = None
     
     def is_empty(self):
         return super().is_empty()
+    
+    def enqueue(self,data):
+        self.insert_at_start(data)
+        
+    def dequeue(self):
+        self.delete_last()
+        
+    def get_front(self):
+        if not self.is_empty():
+            return self.start.item
+        else:
+            raise IndexError("queue is empty")
+    
+    def get_last(self):
+        if not self.is_empty():
+            temp = self.start
+            while temp.next is not None:
+                temp=temp.next
+            return temp.item
+        else:
+            raise IndexError("queue is empty")
     
