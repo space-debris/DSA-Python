541b14730b4bd8dca14cb031d8108a1b8d938abd
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-01-28 20:37:21 +0530

    deque

---
+++ Deque using doubly linked list.py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self,item=None,prev=None,next=None):
+        self.item=item
+        self.prev=prev
+        self.next=next
+    
+class Deque:
+    def __init__(self):
+        self.front=None #variables
+        self.rear=None
+        self.item_count = 0
+    
+    def is_empty(self):
+        return self.item_count==0
+    
+    def insert_front(self,data):
+        nn=Node(data,None,self.front)
+        if self.is_empty():
+            self.rear=nn
+        else:
+            self.front.prev=nn
+        self.front = nn
+        self.item_count +=1
+    
+    def insert_rear(self,data):
+        nn=Node(data,self.rear)
+        if self.is_empty():
+            self.front=nn
+        else:
+            self.rear.next=nn
+        self.rear=nn
+        self.item_count +=1
+    
+    def delete_front(self):
+        if self.is_empty():
+            raise IndexError("deque is empty")
+        
+        if self.front==self.rear:
+            self.front=None
+            self.rear=None
+        else:
+            self.front=self.front.next
+            self.front.prev=None
+        self.item_count -=1
+        
+    def delete_rear(self):
+        if self.is_empty():
+            raise IndexError("deque is empty")
+        
+        if self.front==self.rear:
+            self.front=None
+            self.rear=None
+        else:
+            self.rear=self.rear.prev
+            self.rear.next=None
+        
