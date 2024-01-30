8866eae876016e827ae9480973725d6875791d0e
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-01-30 20:09:42 +0530

    third

--- recursive solution.py
+++ recursive solution.py
@@ -4,11 +4,16 @@ def naturalnumbers(n):
     if n>0:
         naturalnumbers(n-1)
         print(n,end=" ")
-naturalnumbers(5)
-print()
+#naturalnumbers(5)
 
 def naturalnumbersrev(n):
     if n>0:
         print(n,end=" " )
         naturalnumbersrev(n-1)
-naturalnumbersrev(5)
+#naturalnumbersrev(5)
+
+def oddnatunum(n):
+    if n>0:
+        oddnatunum(n-1)
+        print(2*n-1,end=" ")
+oddnatunum(10)
