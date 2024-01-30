de49af4b4cb6e9a1d0fd9d5b7a79715ccedc1082
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-01-30 20:27:39 +0530

    hmm

---
+++ more recursive prob.py
@@ -0,0 +1,21 @@
+#sum of first n natural numbers
+def sumofnat(n):
+     if n==1:
+         return 1
+     return n+sumofnat(n-1)
+print(sumofnat(10))
+
+#sum of first n odd natural numbers
+def sumofoddnat(n):
+    if n==1:
+        return 1
+    return 2*n-1 +sumofoddnat(n-1)
+print(sumofoddnat(10))
+
+def sumofevennat(n):
+    if n==1: #first even number
+        return 2
+    return 2*n +sumofevennat(n-1)
+print(sumofevennat(10))
+
+# factorial
