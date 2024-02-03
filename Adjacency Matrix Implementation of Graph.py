9934b2e1c00eaa3994490e6085710449064c9762
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-02-03 22:59:24 +0530

    add edge

--- Adjacency Matrix Implementation of Graph.py
+++ Adjacency Matrix Implementation of Graph.py
@@ -4,4 +4,9 @@ class Graph:
         self.adj_matrix= [[0]*vno for e in range(vno)]
     
     def add_edge(self,u,v,weight=1):
-        
+        if 0<=u<self.vertex_count and 0<=v<self.vertex_count:
+            self.adj_matrix[u][v]=weight
+            self.adj_matrix[v][u]=weight
+        else:
+            print("invald vertex")
+    
