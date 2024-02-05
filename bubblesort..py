3fd477b6201ff05d9535cffc163e38cd91817d9b
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-02-05 23:33:04 +0530

    function complete

--- bubblesort..py
+++ bubblesort..py
@@ -1,4 +1,10 @@
 def bubble_sort(data_list):
     for r in range(1,len(data_list)):
-        for i in range(len(data_list)-r)
+        for i in range(len(data_list)-r):
+            if data_list[i]>data_list[i+1]:
+                data_list[i],data_list[i+1]=data_list[i+1],data_list[i]
+
+l= [34,67,12,89,25,50]
+bubble_sort(l)  
+print(l)          
     
