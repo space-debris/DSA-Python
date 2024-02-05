75c841283a7a622763c2918b647ac9f19f7f193c
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-02-05 23:35:01 +0530

    modified bubble sort

--- bubblesort..py
+++ bubblesort..py
@@ -3,8 +3,23 @@ def bubble_sort(data_list):
         for i in range(len(data_list)-r):
             if data_list[i]>data_list[i+1]:
                 data_list[i],data_list[i+1]=data_list[i+1],data_list[i]
+                
+def mod_bubble_sort(data_list):
+    flag=False
+    for r in range(1,len(data_list)):
+        for i in range(len(data_list)-r):
+            if data_list[i]>data_list[i+1]:
+                data_list[i],data_list[i+1]=data_list[i+1],data_list[i]
+                flag=True
+        if not flag: #when flag False
+            break
+
+
+
+
 
 l= [34,67,12,89,25,50]
 bubble_sort(l)  
-print(l)          
+print(l)     
+print(mod_bubble_sort(l))     
     
