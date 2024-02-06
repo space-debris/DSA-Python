35d48a04ba1117761df65a7ad03ef5f64358dcc3
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-02-06 23:32:57 +0530

    function complete

--- selection sort.py
+++ selection sort.py
@@ -4,4 +4,8 @@ def selection_sort(list1):
         smallest=list1[i]
         for j in range(i+1,n):
             if list1[j]<smallest:
-                
+              temp=smallest
+              smallest=list1[j]
+              list1[j]=temp
+    return list1
+  
