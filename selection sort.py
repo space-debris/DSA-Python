62c18d1119b27a562f4c504a1024496cc5d299f0
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-02-06 23:30:00 +0530

    iterating

--- selection sort.py
+++ selection sort.py
@@ -1,3 +1,7 @@
 def selection_sort(list1):
     n=len(list1)
-    
+    for i in range(n):
+        smallest=list1[i]
+        for j in range(i+1,n):
+            if list1[j]<smallest:
+                
