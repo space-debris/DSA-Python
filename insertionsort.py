b84fa09cec0434923b7c2fe34f09d1cb39fc7f40
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-02-07 21:18:33 +0530

    test

--- insertionsort.py
+++ insertionsort.py
@@ -1,11 +1,12 @@
 def insertionsort(mylist):
     for i in range(1,len(mylist)):
-        temp=mylist[i]
         m=i-1
         while m>=0:
             if mylist[m]<mylist[i]:
                 mylist[i],mylist[m]=mylist[m],mylist[i]
+            m-=1
     
     return mylist
-                 
-    
+
+mylist=[45,7,3,12,98,74,52,1,34,49]
+print(insertionsort(mylist))    
