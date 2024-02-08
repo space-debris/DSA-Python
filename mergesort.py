2287fdb1d902756e3cab078493e45adf6385465d
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-02-08 22:22:01 +0530

    final

--- mergesort.py
+++ mergesort.py
@@ -1,14 +1,34 @@
 def mergesort(mylist):
-    
-    if len(mylist)<=1:
-        return mylist
-    
-    else:
-        temp=len(mylist)//2
-        list1=[mylist[x] for x in mylist if x<temp and x>=0]
-        list2=[mylist[x] for x in mylist if x>=temp and x<len(mylist)]
-        mergesort(list1)
-        mergesort(list2)
+    if len(mylist) > 1:
+        mid = len(mylist) // 2
+        leftlist = mylist[:mid]
+        rightlist = mylist[mid:]
 
-mylist=[45,85,41,65,78,98,54,12,58]
-    
+        leftlist = mergesort(leftlist)
+        rightlist = mergesort(rightlist)
+
+        i = j = k = 0
+        while i < len(leftlist) and j < len(rightlist):
+            if leftlist[i] < rightlist[j]:
+                mylist[k] = leftlist[i]
+                i += 1
+            else:
+                mylist[k] = rightlist[j]
+                j += 1
+            k += 1
+
+        while i < len(leftlist):
+            mylist[k] = leftlist[i]
+            k += 1
+            i += 1
+
+        while j < len(rightlist):
+            mylist[k] = rightlist[j]
+            k += 1
+            j += 1
+
+    return mylist
+
+listy = [45, 85, 41, 65, 78, 98, 54, 12, 58]
+sorted_listy = mergesort(listy)
+print(sorted_listy)
