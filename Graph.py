ef61c30fea3572e9e7f3ff202d0396597a258232
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-05-02 23:39:50 +0530

    graph using dict and list

---
+++ Graph.py
@@ -0,0 +1,23 @@
+class Graph:
+    def __init__(self,gdict=None):
+        if gdict is None:
+            gdict={}
+        self.gdict=gdict
+    
+    def addEdge(self,vertex,edge):
+        self.gdict[vertex].append(edge)
+    
+testdict={
+    "a":["b","c"],
+    "b":["a","d","e"],
+    "c":["a","e"],
+    "d":["b","e","f"],
+    "e":["d","f"],
+    "f":["d","e"]
+}
+
+graph = Graph(testdict)
+print(graph.gdict)
+print(graph.gdict["e"])
+graph.addEdge("e","c")
+print(graph.gdict["e"])
